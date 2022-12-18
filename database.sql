cb5efc41194438a3f205af2ac94fb1d7b9d3078c
dani <dhaniardian005@gmail.com>
2022-12-18 22:42:28 +0700

    benerin run dan database

--- database.sql
+++ database.sql
@@ -14,9 +14,47 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+-- Dumping structure for table bapenda.m_black_list
+CREATE TABLE IF NOT EXISTS `m_black_list` (
+  `id_black_list` bigint(20) NOT NULL AUTO_INCREMENT,
+  `active` enum('Y','N') NOT NULL DEFAULT 'Y',
+  `phone_number` varchar(15) NOT NULL,
+  `notes` varchar(255) NOT NULL,
+  `created_datetime` datetime NOT NULL,
+  `user_created` int(11) NOT NULL,
+  `update_datetime` datetime NOT NULL,
+  `user_updated` int(11) NOT NULL,
+  PRIMARY KEY (`id_black_list`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 -- Dumping data for table bapenda.m_black_list: ~0 rows (approximately)
 
+-- Dumping structure for table bapenda.m_extension
+CREATE TABLE IF NOT EXISTS `m_extension` (
+  `id_extension` int(11) NOT NULL AUTO_INCREMENT,
+  `extension` varchar(10) NOT NULL,
+  `active` enum('Y','N') NOT NULL DEFAULT 'Y',
+  `created_datetime` datetime NOT NULL,
+  `user_created` int(11) NOT NULL,
+  `update_datetime` datetime NOT NULL,
+  `user_updated` int(11) NOT NULL,
+  PRIMARY KEY (`id_extension`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 -- Dumping data for table bapenda.m_extension: ~0 rows (approximately)
+
+-- Dumping structure for table bapenda.m_group
+CREATE TABLE IF NOT EXISTS `m_group` (
+  `id_group` int(11) NOT NULL AUTO_INCREMENT,
+  `group_name` varchar(50) NOT NULL DEFAULT '0',
+  `group_desc` varchar(100) NOT NULL,
+  `active` enum('Y','N') NOT NULL DEFAULT 'Y',
+  `created_datetime` datetime NOT NULL,
+  `user_created` int(11) NOT NULL,
+  `update_datetime` datetime NOT NULL,
+  `user_updated` int(11) NOT NULL,
+  PRIMARY KEY (`id_group`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Dumping data for table bapenda.m_group: ~3 rows (approximately)
 INSERT IGNORE INTO `m_group` (`id_group`, `group_name`, `group_desc`, `active`, `created_datetime`, `user_created`, `update_datetime`, `user_updated`) VALUES
@@ -24,9 +62,40 @@ INSERT IGNORE INTO `m_group` (`id_group`, `group_name`, `group_desc`, `active`, 
 	(3, 'test12', 'test12', 'Y', '2022-12-18 01:48:06', 0, '2022-12-18 11:19:33', 1),
 	(4, 'test3', 'test', 'Y', '2022-12-18 11:24:32', 1, '0000-00-00 00:00:00', 0);
 
+-- Dumping structure for table bapenda.m_group menu
+CREATE TABLE IF NOT EXISTS `m_group menu` (
+  `id_group_menu` int(11) NOT NULL AUTO_INCREMENT,
+  `id_group` int(11) NOT NULL,
+  `id_menu` int(11) NOT NULL,
+  `active` enum('Y','N') NOT NULL DEFAULT 'Y',
+  `created_datetime` datetime NOT NULL,
+  `user_created` int(11) NOT NULL,
+  PRIMARY KEY (`id_group_menu`),
+  KEY `id_group` (`id_group`),
+  KEY `id_menu` (`id_menu`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 -- Dumping data for table bapenda.m_group menu: ~0 rows (approximately)
 
--- Dumping data for table bapenda.m_menu: ~11 rows (approximately)
+-- Dumping structure for table bapenda.m_menu
+CREATE TABLE IF NOT EXISTS `m_menu` (
+  `id_menu` int(11) NOT NULL AUTO_INCREMENT,
+  `menu_name` varchar(100) NOT NULL,
+  `menu_desc` varchar(100) NOT NULL,
+  `menu_url` varchar(255) NOT NULL,
+  `parent_id` int(11) NOT NULL,
+  `icon` varchar(100) NOT NULL,
+  `order_menu` int(11) NOT NULL DEFAULT 0,
+  `active` enum('Y','N') NOT NULL DEFAULT 'Y',
+  `created_datetime` datetime NOT NULL,
+  `user_created` int(11) NOT NULL,
+  `update_datetime` datetime NOT NULL,
+  `user_updated` int(11) NOT NULL,
+  PRIMARY KEY (`id_menu`),
+  KEY `parent_id` (`parent_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table bapenda.m_menu: ~12 rows (approximately)
 INSERT IGNORE INTO `m_menu` (`id_menu`, `menu_name`, `menu_desc`, `menu_url`, `parent_id`, `icon`, `order_menu`, `active`, `created_datetime`, `user_created`, `update_datetime`, `user_updated`) VALUES
 	(1, 'Settings', 'Menu setting', '', 0, 'fa-solid fa-gear', 2, 'Y', '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', 0),
 	(2, 'Group', 'Group', '/group', 1, 'fa-solid fa-bars-sort', 4, 'Y', '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', 0),
@@ -41,9 +110,61 @@ INSERT IGNORE INTO `m_menu` (`id_menu`, `menu_name`, `menu_desc`, `menu_url`, `p
 	(11, 'Dashboard', 'Dashboard', '/dashboard', 0, 'fa-solid fa-gauge', 0, 'Y', '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', 0),
 	(12, 'Dashboard Agent', 'Dashboard Agent', '/dashboard-agent', 0, 'fa-solid fa-gauge', 1, 'Y', '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', 0);
 
+-- Dumping structure for table bapenda.m_phone_book
+CREATE TABLE IF NOT EXISTS `m_phone_book` (
+  `id_phone_book` bigint(20) NOT NULL AUTO_INCREMENT,
+  `phone_name` varchar(100) NOT NULL DEFAULT '0',
+  `phone_number` varchar(15) NOT NULL,
+  `notes` varchar(255) NOT NULL,
+  `created_datetime` datetime NOT NULL,
+  `user_created` int(11) NOT NULL,
+  `update_datetime` datetime NOT NULL,
+  `user_updated` int(11) NOT NULL,
+  `active` enum('Y','N') NOT NULL DEFAULT 'Y',
+  PRIMARY KEY (`id_phone_book`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 -- Dumping data for table bapenda.m_phone_book: ~0 rows (approximately)
 
+-- Dumping structure for table bapenda.m_sitemap
+CREATE TABLE IF NOT EXISTS `m_sitemap` (
+  `id_sitemap` int(11) NOT NULL AUTO_INCREMENT,
+  `id_group` int(11) NOT NULL,
+  `hirarcy_ordered` int(11) NOT NULL,
+  `active` enum('Y','N') NOT NULL DEFAULT 'Y',
+  `created_datetime` datetime NOT NULL,
+  `user_created` int(11) NOT NULL,
+  `update_datetime` datetime NOT NULL,
+  `user_updated` int(11) NOT NULL,
+  PRIMARY KEY (`id_sitemap`) USING BTREE,
+  KEY `id_group` (`id_group`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 -- Dumping data for table bapenda.m_sitemap: ~0 rows (approximately)
+
+-- Dumping structure for table bapenda.m_users
+CREATE TABLE IF NOT EXISTS `m_users` (
+  `id_user` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id_group` int(11) NOT NULL,
+  `id_extension` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `photo` varchar(255) NOT NULL,
+  `ages` int(11) NOT NULL,
+  `last_login` datetime NOT NULL,
+  `last_logout` datetime NOT NULL,
+  `created_datetime` datetime NOT NULL,
+  `user_created` int(11) NOT NULL,
+  `update_datetime` datetime NOT NULL,
+  `user_updated` int(11) NOT NULL,
+  `parent_user` int(11) NOT NULL,
+  `active` enum('Y','N') DEFAULT 'Y',
+  PRIMARY KEY (`id_user`),
+  KEY `id_group` (`id_group`),
+  KEY `id_extension` (`id_extension`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Dumping data for table bapenda.m_users: ~0 rows (approximately)
 
