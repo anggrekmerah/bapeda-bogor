2af1a568c81c058ac8c1c0ade9898df24db6371c
dani <dhaniardian005@gmail.com>
2022-12-18 22:45:42 +0700

    benerin index

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-var menuClass = require('../config/navbar_model')
 
 /* GET home page. */
 router.get('/',  async (req, res, next) => {
 
-  const classMenus = new menuClass()
-
-  var refactorMenu = await classMenus.refactorMenu()
-
-  res.render('index', { title: 'Express', 'menu': classMenus.bikinMenu(refactorMenu) });
+  res.render('dashboard/dashboard', { controller: 'dashboard', title: 'Dashboard', 'menu': req.menus });
 
 });
 
